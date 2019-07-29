573cffad6095e54887c2e16d32871c2cd8b4687c
Gianfranco <gianfrancobtl@gmail.com>
2019-07-29 20:51:12 -0300

    Se deployó la app en Firebase y se agregó la información del club a Sobre Nosotros

--- src/pages/ProximosPartidos.vue
+++ src/pages/ProximosPartidos.vue
@@ -11,6 +11,8 @@
             <a class="dropdown-item" href="#">U2</a>
             <a class="dropdown-item" href="#">U3</a>
             <a class="dropdown-item" href="#">U4</a>
+            <a class="dropdown-item" href="#">U5</a>
+            <a class="dropdown-item" href="#">U6</a>
           </base-dropdown>
         </template>
       </card>
@@ -22,23 +24,12 @@
               <i class="nc-icon nc-notes"></i>
             </div>
             <div slot="content">
-              <p class="card-category">18:00</p>
-              <h4 class="card-title">U1 vs. U2</h4>
+              <p class="card-category">09:30 a.m.</p>
+              <h4 class="card-title">U1 vs. U4</h4>
             </div>
             <div slot="footer">
               <i class="fa fa-calendar-o"></i>1° de septiembre
-              <button type="button" class="btn btn-light">Ver mapa</button>
             </div>
-              <gmap-map
-                id="map"
-                :center="center"
-                :zoom="13"
-                :options="options"
-                map-type-id="terrain"
-              >
-                <gmap-marker :position="center"></gmap-marker>
-              </gmap-map>
-            
           </stats-card>
         </div>
 
@@ -48,18 +39,11 @@
               <i class="nc-icon nc-notes"></i>
             </div>
             <div slot="content">
-              <p class="card-category">20:00</p>
-              <h4 class="card-title">U3 vs. U4</h4>
+              <p class="card-category">01:00 p.m.</p>
+              <h4 class="card-title">U3 vs. U2</h4>
             </div>
             <div slot="footer">
-              <i class="fa fa-calendar-o"></i>2° de septiembre
-            </div>
-            <div class="text-center">
-              <button
-                type="submit"
-                class="btn btn-info btn-fill float-right"
-                @click.prevent="updateProfile"
-              >Update Profile</button>
+              <i class="fa fa-calendar-o"></i>1° de septiembre
             </div>
           </stats-card>
         </div>
@@ -70,11 +54,11 @@
               <i class="nc-icon nc-notes"></i>
             </div>
             <div slot="content">
-              <p class="card-category">18:00</p>
-              <h4 class="card-title">U1 vs. U4</h4>
+              <p class="card-category">09:30 a.m.</p>
+              <h4 class="card-title">U5 vs. U6</h4>
             </div>
             <div slot="footer">
-              <i class="fa fa-calendar-o"></i>2 de octubre
+              <i class="fa fa-calendar-o"></i>8 de septiembre
             </div>
           </stats-card>
         </div>
@@ -85,14 +69,24 @@
               <i class="nc-icon nc-notes"></i>
             </div>
             <div slot="content">
-              <p class="card-category">20:00</p>
-              <h4 class="card-title">U2 vs. U3</h4>
+              <p class="card-category">01:00 p.m.</p>
+              <h4 class="card-title">U6 vs. U1</h4>
             </div>
             <div slot="footer">
-              <i class="fa fa-calendar-o"></i>5 de octubre
+              <i class="fa fa-calendar-o"></i>8 de septiembre
             </div>
           </stats-card>
         </div>
+      </div>
+      <div class="row">
+        <iframe
+          src="https://calendar.google.com/calendar/embed?src=kdvaoqjhm4915cfik88bsd3gr4%40group.calendar.google.com&ctz=America%2FArgentina%2FBuenos_Aires"
+          style="border: 0"
+          width="800"
+          height="600"
+          frameborder="0"
+          scrolling="no"
+        ></iframe>
       </div>
     </div>
   </div>
@@ -102,66 +96,87 @@
 import ChartCard from "src/components/Cards/ChartCard.vue";
 import StatsCard from "src/components/Cards/StatsCard.vue";
 import LTable from "src/components/Table.vue";
-import {API_KEY} from './Maps/API_KEY'
-  import Vue from 'vue'
-  import * as VueGoogleMaps from 'vue2-google-maps'
-  Vue.use(VueGoogleMaps, {
-    load: {
-      key: API_KEY
-    }
-  })
-  export default {
-    components: {
+import { API_KEY } from "./Maps/API_KEY";
+import Vue from "vue";
+import * as VueGoogleMaps from "vue2-google-maps";
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: API_KEY
+  }
+});
+export default {
+  components: {
     StatsCard
   },
-    data () {
-      return {
-        center: {
-          lat: 40.748817,
-          lng: -73.985428
-        },
-        options: {
-          styles: [{
-            'featureType': 'water',
-            'stylers': [{'saturation': 43}, {'lightness': -11}, {'hue': '#0088ff'}]
-          }, {
-            'featureType': 'road',
-            'elementType': 'geometry.fill',
-            'stylers': [{'hue': '#ff0000'}, {'saturation': -100}, {'lightness': 99}]
-          }, {
-            'featureType': 'road',
-            'elementType': 'geometry.stroke',
-            'stylers': [{'color': '#808080'}, {'lightness': 54}]
-          }, {
-            'featureType': 'landscape.man_made',
-            'elementType': 'geometry.fill',
-            'stylers': [{'color': '#ece2d9'}]
-          }, {
-            'featureType': 'poi.park',
-            'elementType': 'geometry.fill',
-            'stylers': [{'color': '#ccdca1'}]
-          }, {
-            'featureType': 'road',
-            'elementType': 'labels.text.fill',
-            'stylers': [{'color': '#767676'}]
-          }, {
-            'featureType': 'road',
-            'elementType': 'labels.text.stroke',
-            'stylers': [{'color': '#ffffff'}]
-          }, {'featureType': 'poi', 'stylers': [{'visibility': 'off'}]}, {
-            'featureType': 'landscape.natural',
-            'elementType': 'geometry.fill',
-            'stylers': [{'visibility': 'on'}, {'color': '#b8cb93'}]
-          }, {'featureType': 'poi.park', 'stylers': [{'visibility': 'on'}]}, {
-            'featureType': 'poi.sports_complex',
-            'stylers': [{'visibility': 'on'}]
-          }, {'featureType': 'poi.medical', 'stylers': [{'visibility': 'on'}]}, {
-            'featureType': 'poi.business',
-            'stylers': [{'visibility': 'simplified'}]
-          }]
-        }
+  data() {
+    return {
+      center: {
+        lat: 40.748817,
+        lng: -73.985428
+      },
+      options: {
+        styles: [
+          {
+            featureType: "water",
+            stylers: [
+              { saturation: 43 },
+              { lightness: -11 },
+              { hue: "#0088ff" }
+            ]
+          },
+          {
+            featureType: "road",
+            elementType: "geometry.fill",
+            stylers: [
+              { hue: "#ff0000" },
+              { saturation: -100 },
+              { lightness: 99 }
+            ]
+          },
+          {
+            featureType: "road",
+            elementType: "geometry.stroke",
+            stylers: [{ color: "#808080" }, { lightness: 54 }]
+          },
+          {
+            featureType: "landscape.man_made",
+            elementType: "geometry.fill",
+            stylers: [{ color: "#ece2d9" }]
+          },
+          {
+            featureType: "poi.park",
+            elementType: "geometry.fill",
+            stylers: [{ color: "#ccdca1" }]
+          },
+          {
+            featureType: "road",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#767676" }]
+          },
+          {
+            featureType: "road",
+            elementType: "labels.text.stroke",
+            stylers: [{ color: "#ffffff" }]
+          },
+          { featureType: "poi", stylers: [{ visibility: "off" }] },
+          {
+            featureType: "landscape.natural",
+            elementType: "geometry.fill",
+            stylers: [{ visibility: "on" }, { color: "#b8cb93" }]
+          },
+          { featureType: "poi.park", stylers: [{ visibility: "on" }] },
+          {
+            featureType: "poi.sports_complex",
+            stylers: [{ visibility: "on" }]
+          },
+          { featureType: "poi.medical", stylers: [{ visibility: "on" }] },
+          {
+            featureType: "poi.business",
+            stylers: [{ visibility: "simplified" }]
+          }
+        ]
       }
-    }
+    };
   }
-
+};
 </script>
--- src/pages/SobreNosotros.vue
+++ src/pages/SobreNosotros.vue
@@ -4,7 +4,22 @@
       <div class="row">
         <div class="col-12">
           <card>
-
+            <h3 class="card-title">Mission</h3>
+            <p
+              class="card-category"
+            >To support young athletes living in Chicago's northside neighborhoods, who have an interest in learning and playing soccer, with opportunities to learn and practice skills related to the game of soccer, specifically those skills around team cooperation and good sportsmanship.</p>
+          </card>
+          <card>
+            <h3 class="card-title">Vision</h3>
+            <p
+              class="card-category"
+            >The Northside Youth Soccer League aspires to develop strong, well-rounded, and mindful athletes through the building of character, self-discipline, and leadership.</p>
+          </card>
+          <card>
+            <h3 class="card-title">General Information</h3>
+            <p
+              class="card-category"
+            >The Northside Youth Soccer League was established in 1996 to provide athletes residing in Chicago's northside neighborhoods an environment in which to learn and play soccer. To be a member of NYSL, you must be between the ages of 4 - 12 and reside in a Chicago northside neighborhood. NYSL is run by a small full-time staff, and relies on the generous volunteer time of parents and previous league members.</p>
           </card>
         </div>
       </div>
@@ -12,15 +27,13 @@
   </div>
 </template>
 <script>
-  import Card from 'src/components/Cards/Card.vue'
+import Card from "src/components/Cards/Card.vue";
 
-  export default {
-    components: {
-      Card
-    }
+export default {
+  components: {
+    Card
   }
-
+};
 </script>
 <style>
-
 </style>
